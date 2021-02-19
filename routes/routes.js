c5e7d4f3b601c27ee2224238b246e3c9874412d2
Rand Hale <randluca@gmail.com>
2021-02-18 18:49:54 -0800

    write skeleton api routes in routes/routes.js

---
+++ routes/routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", ({ body }, res) => {
+    
+});
+
+router.get("/stats", (req, res) => {
+
+});
+
+router.get("/exercise", (req, res) => {
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.use(require("./routes/routes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
